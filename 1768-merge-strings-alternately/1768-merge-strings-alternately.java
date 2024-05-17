04945be7a46ec9dd5f341bcab86ce427a0256d72
Doritosch <als000825@naver.com>
2024-05-17 14:19:50 +0900

    Time: 10 ms (8.75%), Space: 42.8 MB (13.56%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,25 +1,14 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        // 두 word의 길이를 비교하여 짧은 것을 기준으로 merge하도록 하자.
-        int word1Length = word1.length();
-        int word2Length = word2.length();
-        String word = "";
-        int i=0;
-        if(word1Length <= word2Length){
-            for(i=0; i<word1Length; i++){
-                word = word + word1.charAt(i);
-                word = word + word2.charAt(i);
-            }
-            word = word + word2.substring(i, word2.length());
-        }
-        else{
-            for(i=0; i<word2Length; i++){
-                word = word + word1.charAt(i);
-                word = word + word2.charAt(i);
-            }
-            word = word + word1.substring(i, word1.length());
+        int minLength = Math.min(word1.length(), word2.length());
+        String result = "";
+        int i;
+        for(i=0; i<minLength; i++){
+            result += word1.charAt(i);
+            result += word2.charAt(i);
         }
 
-        return word;     
+        result += word1.substring(i) + word2.substring(i);
+        return result;
     }
 }
